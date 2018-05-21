dd40b4531d346c938d666949991ad2a722389952
Vinicius Horewicz <vinicius@horewi.cz>
2018-05-21 12:04:32 +0100

    Update iTerm2 config

--- iterm2/install.sh
+++ iterm2/install.sh
@@ -1,7 +1,7 @@
 defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$ZSH/iterm2"
 defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -int 1
 
-mkdir -p "~/Library/Application Support/iTerm2/"
-rm -rf "~/Library/Application Support/iTerm2/DynamicProfiles"
+mkdir -p "$HOME/Library/Application Support/iTerm2/"
+rm -rf "$HOME/Library/Application Support/iTerm2/DynamicProfiles"
 
 ln -sfhF "$ZSH/iterm2/profiles" "$HOME/Library/Application Support/iTerm2/DynamicProfiles"
