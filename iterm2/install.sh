47c6c8bcfe134566b149cb4f80974890b0bbe130
Vinicius Horewicz <vinicius@horewi.cz>
2018-05-21 12:49:45 +0100

    Setup iTerm2 dynamic profiles

--- iterm2/install.sh
+++ iterm2/install.sh
@@ -2,6 +2,5 @@ defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$ZSH/iterm2"
 defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -int 1
 
 mkdir -p "$HOME/Library/Application Support/iTerm2/"
-rm -rf "$HOME/Library/Application Support/iTerm2/DynamicProfiles"
-
-ln -sfhF "$ZSH/iterm2/profiles" "$HOME/Library/Application Support/iTerm2/DynamicProfiles"
+rm -rf "$HOME/Library/Application Support/iTerm2/DynamicProfiles" && \
+  ln -s "$ZSH/iterm2/profiles" "$HOME/Library/Application Support/iTerm2/DynamicProfiles"
