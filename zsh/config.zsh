de1245e4521c44e93748450fa4b64ecff667cbc4
Vinicius Horewicz <vinicius@horewi.cz>
2018-05-21 15:32:57 +0100

    Improved history search

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,5 +1,6 @@
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
+export PATH="/usr/local/opt/openssl/bin:$PATH"
 
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
@@ -12,3 +13,10 @@ setopt HIST_IGNORE_ALL_DUPS
 setopt HIST_REDUCE_BLANKS
 setopt SHARE_HISTORY
 setopt CORRECT
+
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search
+bindkey "^[[B" down-line-or-beginning-search
