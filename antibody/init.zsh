a1831480a59882327cd497422628f61dd123d13b
Vinicius Horewicz <vinicius@horewi.cz>
2018-05-21 21:45:48 +0100

    Change antibody cache path

--- antibody/init.zsh
+++ antibody/init.zsh
@@ -1,1 +1,3 @@
+export ANTIBODY_HOME="$HOME/.cache/antibody"
+
 source "$HOME/.zsh_plugins.sh"
--- antibody/install.sh
+++ antibody/install.sh
@@ -1,2 +1,4 @@
+export ANTIBODY_HOME="$HOME/.cache/antibody"
+
 antibody bundle < "$ZSH/antibody/bundles.txt" > "$HOME/.zsh_plugins.sh"
 antibody update
