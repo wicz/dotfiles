9b2bd7283a3cfb375f5004600395fad10ed251ef
Vinicius Horewicz <vinicius@horewi.cz>
2018-05-21 15:41:13 +0100

    Use zsh-history-substring-search instead

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -14,9 +14,5 @@ setopt HIST_REDUCE_BLANKS
 setopt SHARE_HISTORY
 setopt CORRECT
 
-autoload -U up-line-or-beginning-search
-autoload -U down-line-or-beginning-search
-zle -N up-line-or-beginning-search
-zle -N down-line-or-beginning-search
-bindkey "^[[A" up-line-or-beginning-search
-bindkey "^[[B" down-line-or-beginning-search
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
